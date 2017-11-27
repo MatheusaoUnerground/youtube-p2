58dc01a49e4fea4e6e22fde3087db4f81c1f17bc
MatheusãoUnderground <theusventura@gmail.com>
2017-11-27 16:58:06 -0200

    sem alteraçoes, quase a mesma coisa do template
    Youtube - Esportes

---
+++ Esportes.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<link   rel="stylesheet" type="text/css" href="estilotemplate.css">
+<title>Esportes - YouTube</title>
+<body>
+<div id="header"></div>
+<div class="left"></div>
+<div class="right">
+<center><input type="text"></center>
+</div>
+
+
+
+<div class="vertical-menu">
+  <a href="#">Inicio</a>
+  <a href="#">Em alta</a>
+  <a href="#">Histórico</a>
+  <a href="#" class="active">O MELHOR DO YOUTUBE</a>
+  <a href="#">Música</a>
+  <a href="#">Esportes</a>
+  <a href="#">Jogos</a>
+  <a href="#">Filmes</a>
+  <a href="#">Noticias</a>
+  <a href="#">Ao vivo</a>
+  <a href="#">Destaque</a>
+  <a href="#">Video 360º</a>
+  <a href="#" class="active">Procurar canais</a>
+  <a href="#">Faça login agora para ver seus canais e recomendações </a>
+  
+</div>
+</div>
+</body>
+</html>
